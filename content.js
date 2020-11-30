bf7b576a4f7c756f8d4f2e5adc3b0c7e33d60103
Cynthia <cynthia.b.lee@gmail.com>
2020-11-29 21:51:46 -0800

    Fixed bug with ordering of replacements
    Fixed ordering of search and replace so that it doesn't match "mask" when the longer match "masks" would also work and is a better match.

--- content.js
+++ content.js
@@ -8,10 +8,10 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/mask/gi, 'pants');
+            var replacedText = text.replace(/masks/gi, 'pants');
+			    replacedText = replacedText.replace(/Masks/gi, 'Pants');
+			    replacedText = replacedText.replace(/mask/gi, 'pants');
 			    replacedText = replacedText.replace(/Mask/gi, 'Pants');
-			    replacedText = replacedText.replace(/pants/gi, 'pants');
-			    replacedText = replacedText.replace(/pants/gi, 'pants');
 	    if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
