277e5d4803aa411c4cb9772d31dabbb67f42d295
Cynthia <cynthia.b.lee@gmail.com>
2020-11-30 14:09:52 -0800

    Added handling of two-word phrase "face mask"
    Added handling of two-word phrase "face mask," along with capitalized and plural forms. Note to self: there are enough words/phrases now that putting them into an array and using a loop would make a lot more sense than this copy-paste.

--- content.js
+++ content.js
@@ -7,14 +7,23 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/masks/gi, 'pants');
-			    replacedText = replacedText.replace(/Masks/gi, 'Pants');
-			    replacedText = replacedText.replace(/mask/gi, 'pants');
-			    replacedText = replacedText.replace(/Mask/gi, 'Pants');
-	    if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
+		var text = node.nodeValue;
+		var replacedText = text;
+		replacedText = replacedText.replace(/face masks/gi, 'pants');
+		replacedText = replacedText.replace(/Face masks/gi, 'Pants');
+		replacedText = replacedText.replace(/facemasks/gi, 'pants');
+		replacedText = replacedText.replace(/Facemasks/gi, 'Pants');
+		replacedText = replacedText.replace(/face mask/gi, 'pants');
+		replacedText = replacedText.replace(/Face mask/gi, 'Pants');
+		replacedText = replacedText.replace(/facemask/gi, 'pants');
+		replacedText = replacedText.replace(/Facemask/gi, 'Pants');
+		replacedText = replacedText.replace(/masks/gi, 'pants');
+		replacedText = replacedText.replace(/Masks/gi, 'Pants');
+		replacedText = replacedText.replace(/mask/gi, 'pants');
+		replacedText = replacedText.replace(/Mask/gi, 'Pants');
+		if (replacedText !== text) {
+                	element.replaceChild(document.createTextNode(replacedText), node);
+		}
+	}
     }
 }
