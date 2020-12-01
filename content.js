4b2e7023d0474b9f9cd8fa1b8f4d25c503f08cf4
Paul <beriukay+git@gmail.com>
2020-11-30 17:47:18 -0800

    Remove ignore flag to the regex so that the replacement preserves the original word's case

--- content.js
+++ content.js
@@ -1,29 +1,30 @@
 var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+	var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+	for (var j = 0; j < element.childNodes.length; j++) {
+		var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-		var text = node.nodeValue;
-		var replacedText = text;
-		replacedText = replacedText.replace(/face masks/gi, 'pants');
-		replacedText = replacedText.replace(/Face masks/gi, 'Pants');
-		replacedText = replacedText.replace(/facemasks/gi, 'pants');
-		replacedText = replacedText.replace(/Facemasks/gi, 'Pants');
-		replacedText = replacedText.replace(/face mask/gi, 'pants');
-		replacedText = replacedText.replace(/Face mask/gi, 'Pants');
-		replacedText = replacedText.replace(/facemask/gi, 'pants');
-		replacedText = replacedText.replace(/Facemask/gi, 'Pants');
-		replacedText = replacedText.replace(/masks/gi, 'pants');
-		replacedText = replacedText.replace(/Masks/gi, 'Pants');
-		replacedText = replacedText.replace(/mask/gi, 'pants');
-		replacedText = replacedText.replace(/Mask/gi, 'Pants');
-		if (replacedText !== text) {
-                	element.replaceChild(document.createTextNode(replacedText), node);
+		if (node.nodeType === 3) {
+			var text = node.nodeValue;
+			var replacedText = text;
+			replacedText = replacedText.replace(/face masks/g, 'pants');
+			replacedText = replacedText.replace(/Face masks/g, 'Pants');
+			replacedText = replacedText.replace(/facemasks/g, 'pants');
+			replacedText = replacedText.replace(/Facemasks/g, 'Pants');
+			replacedText = replacedText.replace(/face mask/g, 'pants');
+			replacedText = replacedText.replace(/Face mask/g, 'Pants');
+			replacedText = replacedText.replace(/facemask/g, 'pants');
+			replacedText = replacedText.replace(/Facemask/g, 'Pants');
+			replacedText = replacedText.replace(/masks/g, 'pants');
+			replacedText = replacedText.replace(/Masks/g, 'Pants');
+			replacedText = replacedText.replace(/mask/g, 'pants');
+			replacedText = replacedText.replace(/Mask/g, 'Pants');
+
+			if (replacedText !== text) {
+				element.replaceChild(document.createTextNode(replacedText), node);
+			}
 		}
 	}
-    }
 }
